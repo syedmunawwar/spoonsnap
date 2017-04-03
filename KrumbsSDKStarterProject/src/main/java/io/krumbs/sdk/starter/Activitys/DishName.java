c34b4d4984f61d520fb10bbaa770e1d3a8a19036
syed munawwar <syedmunawwar990@gmail.com>
2017-04-03 11:37:56 +0530

    second commit

--- KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/Activitys/DishName.java
+++ KrumbsSDKStarterProject/src/main/java/io/krumbs/sdk/starter/Activitys/DishName.java
@@ -1,7 +1,10 @@
 package io.krumbs.sdk.starter.Activitys;
 
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 import io.krumbs.sdk.starter.R;
 
@@ -9,9 +12,11 @@ import io.krumbs.sdk.starter.R;
  * Created by SYED on 03-04-2017.
  */
 public class DishName extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.dish_name);
+
     }
 }
